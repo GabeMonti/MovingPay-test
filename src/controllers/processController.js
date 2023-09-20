a61e8182df1dc5dbb78c1eaa8560c1756489fd20
Gabriel Montibeller <gabriel.montibeller@gmail.com>
2023-09-20 02:48:16 -0300

    minnor validation fix

--- src/controllers/processController.js
+++ src/controllers/processController.js
@@ -4,9 +4,9 @@ exports.post = async (req, res, next) => {
     let body = req.body
     if (
         (isNaN(body.vl_venda) || body.vl_venda.length === 0) ||
-        (isNaN(body.qt_parcela) || body.qt_parcela.length === 0 || body.qt_parcela === 0) ||
-        (isNaN(body.mdr) || body.mdr.length === 0) ||
-        (isNaN(body.rav) || body.rav.length === 0)
+        (isNaN(body.qt_parcela) || body.qt_parcela.length === 0 || body.qt_parcela === 0 || body.qt_parcela >= 25) ||
+        (isNaN(body.mdr) || body.mdr.length === 0 || body.mdr >= 99.01) ||
+        (isNaN(body.rav) || body.rav.length === 0 || body.rav >= 99.01)
     ) {
         res.status(500).send({message: 'error! - Invalid Parameters', data: []});
     } else {
